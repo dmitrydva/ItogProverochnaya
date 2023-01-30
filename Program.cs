d0522d11c994bf4408a2adb10c93debca1c68e65
Dima <dimdimonish@yandex.ru>
2023-01-30 17:46:27 +0400

    added program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите строки (элементы массива) через запятую:");
+string? line = Console.ReadLine();
+if (!string.IsNullOrEmpty(line))
+{
+    string[] input = line.Split(',');
+    string[] result = new string[0];
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= 3)
+        {
+            Array.Resize(ref result, result.Length + 1);
+            result[result.Length - 1] = input[i];
+        }
+    }
+    if (result.Length != 0) Console.WriteLine($"Новый массив: {String.Join(", ", result)}");
+    else Console.WriteLine("В исходном массиве нет строк длиной меньше или равно 3");
+}
+else Console.WriteLine("Исходный массив пуст");
+
+
